9186e1988d0c5df6cc178f8ef32d6ddec4eb6eca
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-06-20 17:02:18 +0530

    Fixed the usermodel

--- backend/src/model/User.model.ts
+++ backend/src/model/User.model.ts
@@ -1,14 +1,22 @@
-import { Schema, model, Types } from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
-interface User {
+interface IUser {
   name: string;
   email: string;
   password: string;
 }
 
-const userSchema = new Schema<User>({
+interface IUserMethods {
+  isPasswordCorrect(password: string): Promise<boolean>;
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
+}
+
+type UserDocument = Document & IUser & IUserMethods;
+
+const userSchema = new Schema<IUser, {}, {}, IUserMethods>({
   name: {
     type: String,
     required: true,
@@ -27,14 +35,13 @@ const userSchema = new Schema<User>({
   },
 });
 
-userSchema.pre('save', async function () {
-  if (!this.isModified('password')) return;
+userSchema.pre<UserDocument>('save', async function (next) {
+  if (!this.isModified('password')) return ;
 
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-  } catch (error) {
-    throw error;
+  } catch (error: any) {
   }
 });
 
@@ -42,34 +49,20 @@ userSchema.methods.isPasswordCorrect = async function (password: string): Promis
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken = async function () {
-  const accessTokenData = {
-    _id: this._id,
-    email: this.email,
-    username: this.username,
-    fullName: this.fullName,
-  };
-
-  const accessToken = jwt.sign(accessTokenData, process.env.ACCESS_TOKEN_GENERATOR as string, {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY as string,
-  });
-
-  return accessToken;
+userSchema.methods.generateAccessToken = function (): string {
+  return jwt.sign(
+    { _id: this._id, email: this.email },
+    process.env.ACCESS_TOKEN_GENERATOR as string,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY as any }
+  );
 };
 
-userSchema.methods.generateRefreshToken = async function () {
-  const refreshTokenData = {
-    _id: this._id,
-    email: this.email,
-    username: this.username,
-    fullName: this.fullName,
-  };
-
-  const refreshToken = jwt.sign(refreshTokenData, process.env.REFRESH_TOKEN_GENERATOR as string, {
-    expiresIn: process.env.REFRESH_TOKEN_EXPIRY as string,
-  });
-
-  return refreshToken;
+userSchema.methods.generateRefreshToken = function (): string {
+  return jwt.sign(
+    { _id: this._id },
+    process.env.REFRESH_TOKEN_GENERATOR as string,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY as any }
+  );
 };
 
-export const User = model<User>('User', userSchema);
+export const User = model<IUser, {}, IUserMethods>('User', userSchema);
