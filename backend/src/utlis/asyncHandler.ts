a65078e5516571029606fde641a982e680ae8412
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-06-20 17:02:47 +0530

    Updated asyncHandler for ts

--- backend/src/utlis/asyncHandler.ts
+++ backend/src/utlis/asyncHandler.ts
@@ -1,10 +1,18 @@
-export const asyncHandler = (fn) => async (req,res,next) => {
-    try {
-        return await fn(req, res, next)
-    } catch (error) {
-        return res.status(error.statusCode || 500).json({
-            success: false,
-            message: error.message || "Something went wrong internally."
-        });
+import { Request , Response, NextFunction,} from 'express';
+
+export const asyncHandler = (
+    fn : (req : Request, res : Response, next : NextFunction) => Promise<any>
+): ((req : Request, res : Response, next : NextFunction) => Promise<any>) => {
+    return async (req : Request, res : Response, next : NextFunction) => {
+        try {
+            return await fn (req, res, next)
+        } catch (error : any) {
+            const status = error.statusCode || error.status || 500;
+            console.error(error);
+            return res.status(status).json({
+                success: false,
+                message: error.message || 'Something went wrong',
+            });
+        }
     }
 }
